c5cc75e4de266f4e5e868ee9cb163347543645e3
Rickert Goyvaerts <rickert.goyvaerts@gmail.com>
2020-09-01 18:23:49 +0200

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <Header/>
-    <About/>
-    <contact/>
+    <Header />
+    <About />
+    <contact />
   </div>
 </template>
 
 <script>
-import Header from './components/header.vue'
-import Contact from './contact_me/contact.vue'
-import About from './about_me/about_me.vue'
+import Header from "./components/header.vue";
+import Contact from "./contact_me/contact.vue";
+import About from "./about_me/about_me.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Contact,
-    About
-  }
-}
+    About,
+  },
+};
 </script>
 
 <style>
-  #app{
-    background-image: linear-gradient(-120deg, rgb(197, 206, 255),rgb(255, 235, 255) );  
-  }
+html {
+  scroll-behavior: smooth;
+}
+
+#app {
+  background-image: linear-gradient(
+    -120deg,
+    rgb(197, 206, 255),
+    rgb(255, 235, 255)
+  );
+}
 </style>

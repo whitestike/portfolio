09cdd1fdef909ace8409aaad006bd4561b230bc3
rickert <rickert.goyvaerts@student.kdg.be>
2022-04-25 19:01:13 +0200

    adding folding buttons

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
 import 'antd/dist/antd.css'; 
 
+import react from 'react';
+
 import { Layout, Menu, Avatar} from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 
 const { Header, Content } = Layout;
 
-function App() {
-  return (
-    <Layout className="layout">
-      <Header>
-        <h1>Rickert Goyvaerts</h1>
-        <Menu className='menu' theme="dark">
-          <Menu.Item><h1>CV</h1></Menu.Item>
-          <Menu.Item><h1>projects</h1></Menu.Item>
-        </Menu>
-      </Header>
-      <Content className='main'>
-        <div className='hero'>
-          <div>
-            <h1 className='title'>Hi.</h1>
-            <h1 className='title'>Im Rickert Goyvaerts</h1>
-            <h2>web-dev, web-design, ...</h2>
+
+class App extends react.Component{
+
+  fold(){
+    
+  }
+
+  render(){
+    return (
+      <Layout className="layout">
+        <Header>
+          <h1>Rickert Goyvaerts</h1>
+          <Menu className='menu' theme="dark">
+            <Menu.Item><h1>CV</h1></Menu.Item>
+            <Menu.Item><h1>projects</h1></Menu.Item>
+          </Menu>
+        </Header>
+        <Content className='main'>
+          <div className='hero'>
+            <div>
+              <h1 className='title'>Hi.</h1>
+              <h1 className='title'>Im Rickert Goyvaerts</h1>
+              <h2>web-dev, web-design, ...</h2>
+            </div>
+            <div className='img'>
+              <Avatar shape="square" size={300} icon={<UserOutlined />} />
+            </div>
           </div>
-          <div className='img'>
-            <Avatar shape="square" size={300} icon={<UserOutlined />} />
+        </Content>
+        <Content className='about-me'>
+          <h1 className='title--content'>about me</h1>
+          <div className='flyout--Table'>
+            <h3 className='title--table'>title</h3>
+            <ul className='folded'>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+            </ul>
+            <a onClick={this.fold}><img className='button-arrow' src='arrow-down.png'/></a>
           </div>
-        </div>
-      </Content>
-      <Content className='about-me'>
-        <h1 className='title--content'>about me</h1>
-        <div className='flyout--Table'>
+          <div className='flyout--Table'>
           <h3 className='title--table'>title</h3>
-          <ul>
-            <li>test</li>
-            <li>test</li>
-            <li>test</li>
-            <li>test</li>
-          </ul>
-        </div>
-        <div className='flyout--Table'>
-        <h3  className='title--table'>title</h3>
-          <ul>
-            <li>test</li>
-            <li>test</li>
-            <li>test</li>
-            <li>test</li>
-          </ul>
-        </div>
-      </Content>
-    </Layout>
-  );
+            <ul className='folded'>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+            </ul>
+            <a><img className='button-arrow' src='arrow-down.png'/></a>
+          </div>
+        </Content>
+      </Layout>
+    );
+  }
 }
 
 export default App;
